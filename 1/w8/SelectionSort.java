0617e8bba40ad99058a2d76c10c41aa29ee0e061
Zach Kohlberg <zckohlberg@qu.edu>
2026-03-19 16:45:06 -0400

    secton 1, 3/19

--- 1/w8/SelectionSort.java
+++ 1/w8/SelectionSort.java
@@ -26,6 +26,25 @@ public class SelectionSort {
         }
     }
 
+    public void sortTrack(int[] a) {
+        for (int swapIndex = a.length - 1; swapIndex > 0; swapIndex--) {
+            int indexOfGreatest = 0;
+            for (int index = 1; index <= swapIndex; index++) {
+                // we increment outside the if statement so that we track EVERY comparison, not just
+                // the comparisons that result in true
+                comparisons++;
+                if (a[index] > a[indexOfGreatest]) {
+                    // common mistake is to increment comparisons in here, which only counts the
+                    // comparisons the result in true and ignores the others
+                    indexOfGreatest = index;
+                }
+            }
+
+            swaps++;
+            SortHelper.swap(a, indexOfGreatest, swapIndex);
+        }
+    }
+
     // sort(arr):
     public void sortCommented(int[] a) {
         // for each swapIndex from arr.length - 1 to 0
@@ -51,11 +70,19 @@ public class SelectionSort {
     public static void main(String[] args) {
         SelectionSort sorter = new SelectionSort();
 
-        int[] array = SortHelper.randomArray(10, 1, 100);
+        int[] array = SortHelper.randomArray(10000, 1, 100);
 
-        sorter.sort(array);
+        long start = System.nanoTime();
+
+        sorter.sortTrack(array);
+
+        long end = System.nanoTime();
+
+        long duration = end - start;
+
         System.out.println("Is array sorted? " + SortHelper.isSorted(array));
         System.out.println("Swaps = " + sorter.getSwaps());
         System.out.println("Comparisons = " + sorter.getComparisons());
+        System.out.printf("Time = %d.%03d seconds\n", duration / 1_000_000_000, duration / 1_000_000 % 1_000);
     }
 }
