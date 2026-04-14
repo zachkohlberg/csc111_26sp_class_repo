935ea0aa811a435cbe7caa8a9b20b02c1e0d0141
Zach Kohlberg <zckohlberg@qu.edu>
2026-04-14 18:03:48 -0400

    node practice starter code

---
+++ 2/w12/BTNode.java
@@ -0,0 +1,6 @@
+public class BTNode<T> {
+    // TODO: variables for left and right child
+    // TODO: variable for the data
+
+    // TODO: (optional) constructors to make nodes with data and/or children
+}
---
+++ 2/w12/BTNodeTest.java
@@ -0,0 +1,25 @@
+public class BTNodeTest {
+    // For an example with our regular nodes, check NodeTest from w10
+    public static void main(String[] args) {
+        // if you have a BTNode(T initData) constructor then you can start with:
+        BTNode<String> root = new BTNode<>("D");
+
+        // TODO: create the tree shown below without introducing any more
+        // variables. Just add nodes through the root variable.
+        //
+        //      D
+        //     / \
+        //    /   \
+        //   A     C
+        //  / \     \
+        // B   F     G
+        //    /
+        //   E
+
+        // TODO: print all of the strings stored in the nodes, again without
+        // creating any additional variables. For example, you could print the
+        // F with:
+        //
+        // System.out.println(root.left.right.data);
+    }
+}
