4a6435512228c45eab2d1a1069f5fe2d6acf29c8
Zach Kohlberg <zckohlberg@qu.edu>
2026-04-16 17:51:34 -0400

    BST starter, section 2

---
+++ 2/w12/BST.java
@@ -0,0 +1,29 @@
+public class BST {
+    private BTNode<String> root;
+
+    public BST() {
+        root = null;
+    }
+
+    public void insert(String element) {
+    }
+
+    public boolean contains(String element) {
+    }
+
+    public void inorder() {
+    }
+
+    public void preorder() {
+    }
+
+    public void postorder() {
+    }
+}
+// how we'd start if we were using a comparator to make our BST generic
+// private BTNode<E> root;
+// private Comparator<E> comparator;
+//
+// public BST(Comparator<E> comp) {
+//     comparator = comp;
+// }
